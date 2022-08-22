88afd836a1bbc3045ce7bfbd2829e3e6f38aa9fe
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-21 22:21:18 -0400

    Added content to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
+const dealButton = document.getElementById("deal-button");
+const hitButton = document.getElementById("hit-button");
 const deck = [];
 const suits = ["hearts", "spades", "clubs", "diamonds"];
 const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
@@ -20,4 +22,45 @@ for (let suit of suits) {
 
 window.addEventListener("DOMContentLoaded", () => {
   // Execute after page load
+
 });
+
+function makeElement(elType) {
+  return document.createElement(elType)
+}
+dealButton.addEventListener("click", element => {
+  
+  let cardImg = makeElement("img");
+  cardImg.className = "card-img";
+  cardImg.src = `./images/${ranks[Math.floor(Math.random() * ranks.length)]}_of_${suits[Math.floor(Math.random() * suits.length)]}.png`;
+  playerHand.appendChild(cardImg);
+
+  let cardImg2 = makeElement("img")
+  cardImg2.className = "card-img2";
+  cardImg2.src = `./images/${ranks[Math.floor(Math.random() * ranks.length)]}_of_${suits[Math.floor(Math.random() * suits.length )]}.png`
+  dealerHand.appendChild(cardImg2);
+  
+  let cardImg3 = makeElement("img");
+  cardImg3.className = "card-img3";
+  cardImg3.src = `./images/${ranks[Math.floor(Math.random() * ranks.length)]}_of_${suits[Math.floor(Math.random() * suits.length)]}.png`;
+  playerHand.appendChild(cardImg3);
+
+  let cardImg4 = makeElement("img")
+  cardImg4.className = "card-img4";
+  cardImg4.src = `./images/${ranks[Math.floor(Math.random() * ranks.length)]}_of_${suits[Math.floor(Math.random() * suits.length)]}.png`
+  dealerHand.appendChild(cardImg4);
+  
+}) 
+hitButton.addEventListener("click", () => {
+  
+  let cardImg = makeElement("img");
+  cardImg.className = "card-img";
+  cardImg.src = `./images/${ranks[Math.floor(Math.random() * ranks.length)]}_of_${suits[Math.floor(Math.random() * suits.length)]}.png`;
+  playerHand.appendChild(cardImg);
+
+  let cardImg2 = makeElement("img")
+  cardImg2.className = "card-img2";
+  cardImg2.src = `./images/${ranks[Math.floor(Math.random() * ranks.length)]}_of_${suits[Math.floor(Math.random() * suits.length )]}.png`
+  dealerHand.appendChild(cardImg2);
+
+})
